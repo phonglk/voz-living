21a01c5f7db6e5279c79e90cb9e20b55cea594ad
Phong Le-Khoi <phong@niometrics.com>
2016-03-03 23:16:43 +0800

    youtu.be link fix for youtube

--- js/modules/LinkProcess/SmartLinkRecognition.js
+++ js/modules/LinkProcess/SmartLinkRecognition.js
@@ -19,6 +19,9 @@ define(function() {
                 $this = $(this);
                 href = $this.attr("href");
                 ytb = href.match(/youtube\.com[^\s]+v=([a-zA-Z0-9_-]+)/i);
+                if (ytb === null || ytb.length === 0){
+                    ytb = href.match(/youtu\.be\/([a-zA-Z0-9_-]+)/i);
+                }
                 if (ytb !== null && ytb.length > 0) {
                     $this.attr("data-smartlink", "youtube");
                     $img = $("<div><iframe width='560' height='315' src='https://www.youtube.com/embed/" + ytb[1] + "?rel=0' frameborder='0' allowfullscreen title='Có thể xảy ra sai sót trong việc tự động nhận biết youtube, nếu có xin vui lòng báo lỗi qua pm greans(@vozforum)'></iframe></div>");
